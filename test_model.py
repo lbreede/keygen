6a6a46dbf72bbb6219ac043643b6f945da49119f
lbreede <67719520+lbreede@users.noreply.github.com>
2024-01-16 20:58:54 -0800

    :white_check_mark: Added test_model.py

---
+++ test_model.py
@@ -0,0 +1,11 @@
+from model import Model
+
+
+model = Model()
+
+
+def test_generate_key():
+    # TODO: Add test case for lowercase letters
+    # TODO: Add test case for digits
+    # TODO: Add test case for whitespace
+    pass
