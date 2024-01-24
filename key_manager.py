4b7c0b8bb5671a9dd9abfb199a7f7a8e7e2e37fd
lbreede <67719520+lbreede@users.noreply.github.com>
2024-01-23 17:53:45 -0800

    :fire: Remove Model class in favor for more specialized key manager classes that follow the KeyManager protocol

--- key_manager.py
+++ key_manager.py
@@ -83,51 +83,6 @@ class Win95KeyManager:
         print(f"Validated {iterations:,} keys in {duration:.2f} seconds.")
 
 
-class Model:
-    def __init__(self):
-        self.key_length = 25
-        assert self.key_length % 5 == 0
-
-    def check_key(self, input_data: str, key: str) -> bool:
-        if len(key) < self.key_length:
-            logger.warning("Key %r is too short", self.add_dashes(key))
-            return False
-
-        if len(key) > self.key_length:
-            # This should never happen
-            logger.warning("Key %r is too long", self.add_dashes(key))
-            return False
-
-        if key != self.generate_key(input_data):
-            logger.warning("Key %r is invalid", self.add_dashes(key))
-            return False
-
-        logger.info("Key %r is valid", self.add_dashes(key))
-        return True
-
-    def generate_key(self, input_data: str, add_dashes: bool = False) -> str:
-        logger.debug("Generating key for seed %r", input_data)
-        sha256_hash = hashlib.sha256(input_data.encode()).digest()
-        key = base64.urlsafe_b64encode(sha256_hash).decode("utf-8")
-        key = "".join(char for char in key if char.isalnum())
-        # Highly unlikely that the key is less than 25 characters at this point.
-        # However, if it is, we'll pad it with X's.
-        key = key.ljust(self.key_length, "X")
-        key = key[: self.key_length]
-        key = key.upper()
-        if add_dashes:
-            key = self.add_dashes(key)
-        return key
-
-    def add_dashes(self, text: str) -> str:
-        magic_number = self.key_length // 5
-        for i in range(4):
-            n = (i + 1) * magic_number + i
-            if len(text) > n:
-                text = text[:n] + "-" + text[n:]
-        return text
-
-
 def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
     print(Win95KeyManager().format_key("11aaaaaaa2362457724574511111"))
