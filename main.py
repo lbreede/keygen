e305f2ea5c3109ef1868470805a1caffea126c75
lbreede <67719520+lbreede@users.noreply.github.com>
2024-01-23 17:39:15 -0800

    :recycle: Refactor imports in main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import pathlib
 from PyQt5.QtWidgets import QApplication
 
 from key_manager import Win95KeyManager
-from keygen import KeygenUI
-from keygen import Presenter as KeygenPresenter
+from keygen import KeygenUI, KeygenCtrl
 from software import Presenter as SoftwarePresenter
 from software import View as SoftwareView
 
@@ -23,17 +22,13 @@ def setup_logging() -> None:
 def main() -> None:
     setup_logging()
     logger.info("Starting application")
-
     app = QApplication([])
 
     model = Win95KeyManager()  # Be mindful this is shared between the two views
+    _s = SoftwarePresenter(model, software_view := SoftwareView())
+    _k = KeygenCtrl(model, keygen_view := KeygenUI())
 
-    software_view = SoftwareView()
-    _software_presenter = SoftwarePresenter(model, software_view)
     software_view.move(100, 100)
-
-    keygen_view = KeygenUI()
-    _keygen_presenter = KeygenPresenter(model, keygen_view)
     keygen_view.move(300, 300)
 
     software_view.show()
